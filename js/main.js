d416b15506640f49b6f892bbeccc1d2b4e4cc082
eatspaint <cef2010@gmail.com>
2019-09-02 14:38:48 -0700

    Add waves

--- js/main.js
+++ js/main.js
@@ -2,9 +2,10 @@ import p5 from 'p5';
 import { useColorFire } from './sketches/useColorFire';
 import { useLines } from './sketches/useLines';
 import { useMoire } from './sketches/useMoire';
+import { useWaves } from './sketches/useWaves';
 
 const sketch = (p) => {
-  const [setup, draw] = useMoire(p);
+  const [setup, draw] = useWaves(p);
 
   p.setup = setup;
   p.draw = draw;
