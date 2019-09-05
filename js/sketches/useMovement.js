64b1bdc05aed82a094e23d5596cf23f538c1e56c
eatspaint <cef2010@gmail.com>
2019-09-04 21:27:23 -0700

    Changes to useMovement

--- js/sketches/useMovement.js
+++ js/sketches/useMovement.js
@@ -13,9 +13,8 @@ const initializeMain = (p) => {
 const initializeGFX = (p) => {
   gfx = p.createGraphics(p.windowWidth, p.windowHeight);
   gfx.colorMode(p.HSL);
-  gfx.stroke(0, 0, 0);
+  gfx.stroke(0, 0, 100);
   gfx.strokeWeight(2);
-  gfx.fill(0, 0, 0, 0.05);
   gfx.translate(gfx.width / 2, gfx.height / 2);
   gfx.imageMode(gfx.CENTER);
   gfx.angleMode(gfx.DEGREES);
@@ -34,26 +33,27 @@ const setup = (p) => () => {
 };
 
 const resetBackground = (p) => {
-  gfx.background(15, 20, 50);
+  gfx.background(0, 0, 100);
 };
 
 const loadPrevious = () => {
   gfx.image(previous_gfx, 0, 0);
 };
 
-const SIZE = 1;
-const FIDELITY = 5;
+const SIZE = 0.7;
+const FIDELITY = 360 / 360; // 360 / desired_number_of_walls
+const NOISE_INTENSITY = 0.7;
 const drawNextFrame = (p) => {
   let minDimension = p.windowWidth > p.windowHeight ? p.windowHeight : p.windowWidth;
   let scaledRadius = (minDimension * SIZE) / 2;
-  gfx.fill(0, 0, 100);
   gfx.beginShape();
+  gfx.fill(p.frameCount, 100, 50, 0.1);
   for (let i = 0; (i * FIDELITY) <= 360; i++) {
     let unitX = gfx.cos(i * FIDELITY);
     let unitY = gfx.sin(i * FIDELITY);
     let radius = gfx.noise(
-      (scaledRadius + unitX) * 0.7,
-      (scaledRadius + unitY) * 0.7,
+      (scaledRadius + unitX) * NOISE_INTENSITY,
+      (scaledRadius + unitY) * NOISE_INTENSITY,
       p.frameCount * 0.05,
     ) * scaledRadius;
     gfx.vertex(
@@ -73,13 +73,19 @@ const storeCurrentFrames = (p) => {
   previous_gfx.image(gfx, ...offset(p));
 };
 
-const UNIT_DISTANCE = 50;
+const UNIT_DISTANCE = 10;
 const offset = (p) => {
-  let xOffset = UNIT_DISTANCE * p.sin(p.frameCount);
-  let yOffset = UNIT_DISTANCE * p.cos(p.frameCount);
+  let xOffset = UNIT_DISTANCE * p.sin(p.frameCount * 0.6);
+  let yOffset = UNIT_DISTANCE * p.cos(p.frameCount * 0.6);
   // bitwise or, rounding offsets to avoid blurring
   return [xOffset | 0, yOffset | 0];
 }; 
+
+// const saveFrame = (p) => {
+//   if (p.frameCount <= 180) {
+//     p.saveCanvas(`useMovement${p.frameCount}`);
+//   }
+// }
 
 const draw = (p) => () => {
   resetBackground(p);
@@ -87,6 +93,7 @@ const draw = (p) => () => {
   drawNextFrame(p);
   renderCurrentFrames(p);
   storeCurrentFrames(p);
+  // saveFrame(p);
 };
 
 export const useMovement = (p) => [setup(p), draw(p)];
