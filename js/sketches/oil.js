612803234cbd147f2bb17d53fbf29bac2b63f3f1
eatspaint <cef2010@gmail.com>
2019-11-09 22:13:39 -0800

    update oil

--- js/sketches/oil.js
+++ js/sketches/oil.js
@@ -1,11 +1,15 @@
 export default (p) => {
   const fill = () => {
+    p.background(0);
+    let time = p.frameCount * 0.005;
     for (let x = 0; x <= p.windowWidth; x++) {
       for (let y = 0; y <= p.windowHeight; y++) {
-        let val = p.noise(x * 0.005, y * 0.005);
+        let val = p.noise(x * 0.005, y * 0.005, time);
         let hue = p.map(val, 0, 1, 0, 360 * 45) % 360;
-        p.stroke(hue, 100, 50);
-        p.point(x, y);
+        if ((val > 0.4 && val < 0.6) || val < 0.3 || val > 0.7) {
+          p.stroke(hue, 100, 50);
+          p.point(x, y);
+        }
       }
     }
   };
@@ -13,10 +17,11 @@ export default (p) => {
   p.setup = () => {
     p.colorMode(p.HSL);
     p.createCanvas(p.windowWidth, p.windowHeight);
-    fill();
+    p.frameRate(10);
   };
 
   p.draw = () => {
+    fill();
     p.noLoop();
   };
 };
